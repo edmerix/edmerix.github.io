9f9698feedaf67cee2451723fdd24b58a3d2ed81
edmerix <teddymerricks@hotmail.com>
2021-04-12 18:11:16 -0400

    Better pubmed search to include all papers (and new job title!)

--- core.js
+++ core.js
@@ -324,7 +324,7 @@ core.currency = function(args,trmnl){
 	const date_end = now.toISOString().split('T')[0];
 
 	let xhr = new XMLHttpRequest();
-    
+
 	xhr.open('GET', `https://api.exchangerate.host/timeseries?start_date=${date_start}&end_date=${date_end}&base=${currencies[0]}&symbols=${currencies[1]}`, true);
 	xhr.responseType = 'json';
 	xhr.onload = function(){
@@ -1044,9 +1044,9 @@ core.science = function(args,trmnl){
 	/*
 <div itemscope itemtype="https://schema.org/Person"><a itemprop="sameAs" content="https://orcid.org/0000-0001-8172-3152" href="https://orcid.org/0000-0001-8172-3152" target="orcid.widget" rel="me noopener noreferrer" style="vertical-align:top;"><img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon">https://orcid.org/0000-0001-8172-3152</a></div>
 	*/
-	trmnl.output("@__Ed is currently a Post-Doctoral Research Scientist at <a target='_blank' href='https://www.cuimc.columbia.edu'>Columbia University Medical Center</a> in the <a target='_blank' href='http://www.columbianeurology.org'>Dept. of Neurology</a>\n\
+	trmnl.output("@__Ed is currently an Associate Research Scientist at <a target='_blank' href='https://www.cuimc.columbia.edu'>Columbia University Medical Center</a> in the <a target='_blank' href='http://www.columbianeurology.org'>Dept. of Neurology</a>\n\
 	His research focuses on the activity of populations of single neurons in epilepsy patients during seizures,<br />especially the activity of individual inhibitory cells at seizure onset, and alterations to firing patterns<br />in what's known as the '<i>post-ictal</i>' period.\
-	@__Click to go to his <a target='_blank' href='https://scholar.google.com/citations?user=PnKpxtIAAAAJ&hl=en&oi=ao'>Google Scholar page</a>, <a target='_blank' href='https://www.ncbi.nlm.nih.gov/pubmed/?term=(merricks+em%5Bauthor%5D)'>publications</a>, <a target='_blank' href='https://github.com/edmerix/'>GitHub</a>, <a target='_blank' href='https://www.researchgate.net/profile/Edward_Merricks'>ResearchGate</a>, or <a target='_blank' href='https://orcid.org/0000-0001-8172-3152'><img src='https://orcid.org/sites/default/files/images/orcid_16x16.png' style='width:1em;margin-right:.5em;'' alt='ORCID iD icon'>ORCiD</a>\n\
+	@__Click to go to his <a target='_blank' href='https://scholar.google.com/citations?user=PnKpxtIAAAAJ&hl=en&oi=ao'>Google Scholar page</a>, <a target='_blank' href='https://www.ncbi.nlm.nih.gov/pubmed/?term=(edward+merricks%5Bauthor%5D)'>publications</a>, <a target='_blank' href='https://github.com/edmerix/'>GitHub</a>, <a target='_blank' href='https://www.researchgate.net/profile/Edward_Merricks'>ResearchGate</a>, or <a target='_blank' href='https://orcid.org/0000-0001-8172-3152'><img src='https://orcid.org/sites/default/files/images/orcid_16x16.png' style='width:1em;margin-right:.5em;'' alt='ORCID iD icon'>ORCiD</a>\n\
 	He has code to open <a target='_blank' href='https://github.com/edmerix/openNSx-swift'>NSx neural data files in Swift</a>, do the <a target='_blank' href='https://github.com/edmerix/SplitMerge'>manual stage of spike sorting</a>, or<br /><a target='_blank' href='https://github.com/edmerix/NeuroClass'>analyze populations of single unit data</a>, among others (ask for more).\
 	@__There have been a lot of questions about spike sorting through time recently...\n\
 	Click <a href='SpikeSorting' target='_blank'>here</a> for a (very) quick overview of spike sorting through time");
